0b87badd21a4130e5e36e3e8df800f5621c99361
8490 <ethan@clarusway.com>
2023-03-07 13:12:18 +0300

    update AddModal

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -3,23 +3,40 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-export function AddModal({ show, handleClose }) {
+export function AddModal({ show, handleClose, drName }) {
+  const [patientName, setPatientName] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleClose();
+  };
+  console.log(patientName, date);
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Appointment for ...</Modal.Title>
+          <Modal.Title>
+            Appointment for <span className="text-warning">{drName}</span>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                onChange={(e) => setPatientName(e.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="datetime">
               <Form.Label>Day&Time</Form.Label>
-              <Form.Control type="datetime-local" />
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setDate(e.target.value)}
+              />
             </Form.Group>
 
             <div className="text-center">
